75a7deb6a53c9a7b2c44e044e831964dba3edf57
adwd <adwd@users.noreply.github.com>
2022-02-25 16:19:18 +0900

    ch05

--- ch05/05_rust_example/src/main.rs
+++ ch05/05_rust_example/src/main.rs
@@ -1,7 +1,15 @@
 mod blocking_echo_server;
+mod coroutine;
+mod ioselect;
 mod nonblocking_epoll;
+mod scheduling;
+mod tokio_echo;
 
 fn main() {
-    blocking_echo_server::main();
-    nonblocking_epoll::main();
+    // blocking_echo_server::main();
+    // nonblocking_epoll::main();
+    coroutine::main();
+    scheduling::main();
+    // ioselect::main();
+    tokio_echo::main();
 }
